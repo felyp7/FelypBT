02ad00ccb1a65eed1dc4e19a843d9917d1c5d796
CrucialEnjoyer <janichfilip@seznam.cz>
2022-01-09 21:15:21 +0100

    kekw

--- BOT.js
+++ BOT.js
@@ -1198,6 +1198,7 @@ if(message.startsWith(`'math`)){
 
     if(message.toLowerCase().startsWith("'restart") && user['user-id'] === "162760707") {
         client.say(channel, 'Restarting... ppJump')
+        .then
         process.exit()
     }
 
