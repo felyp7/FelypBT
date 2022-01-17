9c78c234109e8a51ddf74a3967a9f79c5bd681d2
Felyp <janichfilip@seznam.cz>
2022-01-17 09:17:33 +0100

    asd

--- BOT.js
+++ BOT.js
@@ -1286,7 +1286,7 @@ if (message.toLowerCase().startsWith("'rafk")) { //command
             let construct = {
                 id: user['user-id'],
                 reason: afkMessage,
-                time: new Date().getTime() - Date.parse(afkcheck.time)
+                time: new Date().getTime()
             };
             
             client.afk.set(user['user-id'], construct);
