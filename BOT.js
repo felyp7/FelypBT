7510a9aac14e36bc45d3193f83d6f5c6bf2a62ef
Felyp <janichfilip@seznam.cz>
2022-01-19 15:35:04 +0100

    asd

--- BOT.js
+++ BOT.js
@@ -1343,7 +1343,7 @@ if(message.startsWith(`'math`)){
     }
 
 if (channel === '#xqcs_desk_garbage') {
-    if(message == "monkaStop ALARM " && user['user-id'] === "162760707") {
+    if(message == "monkaStop ALARM" && user['user-id'] === "162760707") {
         client.color(array[Math.floor(Math.random() * array.length)])
         client.action(channel, 'TriHard')
     }
