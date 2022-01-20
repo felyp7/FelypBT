d01ce3eaeeaacb32a54fbe01d64857b3faefb485
Felyp <janichfilip@seznam.cz>
2022-01-20 01:19:16 +0100

    asd

--- BOT.js
+++ BOT.js
@@ -766,7 +766,17 @@ if(isModUp) {
             const creation = await got(`https://decapi.me/twitch/creation/${userTarget}`);
                 let creationDate = creation.body
 
-            client.action(channel, `@${user.username} ${userTarget}, Banned: ${isbanned}, Partner: ${isPartner}, Affiliate: ${isAffiliate}, Badge: ${badge}, Avatar: ${avatar}, Color: ${userColor} (${colorName.name.value}), Account created at ${creationDate}, id: ${uid}, bio: ${bio}`)  
+                if (isbanned === 'false') {
+                    client.action(channel, `@${user.username} ${userTarget}, Banned: ${isbanned} ❌, Partner: ${isPartner}, Affiliate: ${isAffiliate}, Badge: ${badge}, Avatar: ${avatar}, Color: ${userColor} (${colorName.name.value}), Account created at ${creationDate}, id: ${uid}, bio: ${bio}`) 
+                    ;return;
+                }
+                
+                if (isbanned === 'true') {
+                    client.action(channel, `@${user.username} ${userTarget}, Banned: ${isbanned} ✅, Partner: ${isPartner}, Affiliate: ${isAffiliate}, Badge: ${badge}, Avatar: ${avatar}, Color: ${userColor} (${colorName.name.value}), Account created at ${creationDate}, id: ${uid}, bio: ${bio}`) 
+                    ;return;
+                }
+
+              
     
             block = true;
             setTimeout(() => {
