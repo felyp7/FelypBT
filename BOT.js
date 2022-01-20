56e7cc0d362d81f52136e2bea1372e1977bf9ce7
Felyp <janichfilip@seznam.cz>
2022-01-20 06:10:51 +0100

    asd

--- BOT.js
+++ BOT.js
@@ -641,7 +641,10 @@ if (message.toLowerCase().startsWith("'ping") && command === 'ping') {
                     responseType: 'json',
                     throwHttpErrors: false
                 })
-                if(!userCheck.body.id) return { reply: `This user does not exist.` }
+                if(!userCheck.body.id) {
+                    client.action(`This user does not exist.`) 
+                ;return;
+                }
 
                 const userData = userCheck.body
                 const userColor = userData.chatColor
