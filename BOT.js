f98950f883e6694e87f49447894cb9218d04d53e
Felyp <janichfilip@seznam.cz>
2022-01-20 01:56:37 +0100

    asd

--- BOT.js
+++ BOT.js
@@ -935,7 +935,7 @@ if(isModUp) {
                 }
                 username = args[0];
             }
-                const userCheck = await got(`https://api.ivr.fi/logs/firstmessage/${channelTarget}/${username}`,{
+                const userCheck = await got(`https://api.ivr.fi/logs/firstmessage/${channel}/${username}`,{
                     responseType: 'json',
                     throwHttpErrors: false
                 })
