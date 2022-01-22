fc568f9aa389f237b44222c6ea3ba50efb9188fa
Felyp <janichfilip@seznam.cz>
2022-01-22 15:44:34 +0100

    asd

--- BOT.js
+++ BOT.js
@@ -1849,7 +1849,7 @@ const me = await api.request({
   authType: "bearer",
 });
 
-console.log(me);
+console.log(me.config);
     
     const token = Spotify_1
 
@@ -1858,7 +1858,7 @@ console.log(me);
           headers: {
           "Accept" : "application/json",
           "Content-Type" : "application/json",
-          "Authorization" : `Bearer ${me} `
+          "Authorization" : `Bearer ${me.config} `
           }
         }
     
