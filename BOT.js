8c2c64ea616a0bd4593204c8f1e97e94b580955d
Felyp <janichfilip@seznam.cz>
2022-01-24 00:07:30 +0100

    asd

--- BOT.js
+++ BOT.js
@@ -1894,7 +1894,7 @@ const options = {
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
 
-  console.log(body.code);
+  console.log(body.url);
 });
 }
         
