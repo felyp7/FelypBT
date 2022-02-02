f612049b0fc4dc0b91e00246c329200a110633c9
Felyp <janichfilip@seznam.cz>
2022-02-02 16:59:12 +0100

    asd

--- BOT.js
+++ BOT.js
@@ -951,7 +951,7 @@ if(isModUp) {
             const userBanned = userData.banned
             const userPartner = userData.partner
             const userAffiliate = userData.affiliate
-            const userBadge = userData.badge.title
+            const userBadge = userData.badge[0].title
             const userBot = userData.bot
 
             const uid = userId
