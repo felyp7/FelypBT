c42149dfd6928ce083c18b6fda4923efbef75f11
Felyp <janichfilip@seznam.cz>
2022-02-03 05:34:32 +0100

    asd

--- BOT.js
+++ BOT.js
@@ -1954,7 +1954,7 @@ if (message.toLowerCase().startsWith("'weather")){
 
         let unix_timestamp2 = JSONObj.sys.sunset
 
-        var date2 = new Date(Date.now());
+        var date2 = new Date(unix_timestamp * 1000);
         var hours2 = date2.getHours();
         var minutes2 = "0" + date2.getMinutes();
         var seconds2 = "0" + date2.getSeconds();
@@ -1965,7 +1965,11 @@ if (message.toLowerCase().startsWith("'weather")){
         var SunRise = setsunRise
         var SunSet = setsunSet  
         
-        console.log(SunSet)
+        var test = d = new Date(value) ;
+        d.setTime( d.getTime() - new Date().getTimezoneOffset()*60*1000 );
+
+        console.log(test)
+
         console.log(channel, `${JSONObj.name}, ${JSONObj.sys.country}: ${JSONObj.main.temp}°C, feels like ${JSONObj.main.feels_like}°C. Weather: ${JSONObj.weather[0].description}. ${deg} Wind speed: ${JSONObj.wind.speed} m/s. Wind gusts up to ${JSONObj.wind.gust} m/s. Humadity: ${JSONObj.main.humidity}%. Air pressure: ${JSONObj.main.pressure} hPa. Sun rises at ${SunRise}, sunset at ${SunSet}.  `)
         
     })
