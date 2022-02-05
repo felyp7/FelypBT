1aa186027640d20f9fcb09f0a83f5bb06f21c594
Felyp <janichfilip@seznam.cz>
2022-02-05 18:13:53 +0100

    asd

--- BOT.js
+++ BOT.js
@@ -56,7 +56,7 @@ client.on("message", async (channel, user, message, self) => {
     if (self) return;
 
 
-    const colors =  [ //rainbow
+    const colors =  [
         "/color #FFFE66", "/color #F9FE66", "/color #F3FE66", "/color #EDFE66", "/color #E7FE66", "/color #E1FE66", "/color #DBFE66", "/color #D5FE66", "/color #CFFE66", "/color #C9FE66",
         "/color #C3FE66", "/color #BDFE66", "/color #B7FE66", "/color #B0FF66", "/color #AAFF66", "/color #A4FF66", "/color #9EFF66", "/color #98FF66", "/color #92FF66", "/color #8CFF66",
         "/color #86FF66", "/color #80FF66", "/color #7AFF66", "/color #74FF66", "/color #6EFF66", "/color #68FF66", 
