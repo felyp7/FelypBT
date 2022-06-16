f49faa057838443238748c18a0d1277a3604096a
felyp <janichfilip@seznam.cz>
2022-06-16 14:31:56 +0200

    xd

--- BotMain/BOT.js
+++ BotMain/BOT.js
@@ -2198,10 +2198,7 @@ if (message.toLowerCase().startsWith("'query")) {
         const WolframAlphaAPI = require('wolfram-alpha-api');
         const waApi = WolframAlphaAPI('9HYJY9-ARQUG4X5EV');
 
-        const data = await got(`https://api.wolframalpha.com/v1/result?appid=9HYJY9-ARQUG4X5EV&i=${args.join(" ")}`,{
-            responseType: 'json',
-            throwHttpErrors: false
-        })
+        const data = await got(`https://api.wolframalpha.com/v1/result?appid=9HYJY9-ARQUG4X5EV&i=${args.join(" ")}`)
         client.action(channel, data)
 
         block = true;
